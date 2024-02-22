2a69ed4ea9734e56b1c655bf7fa0610da1dfdf55
Alekhya <jujjurialekhya123@gmail.com>
2024-02-22 23:55:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let firstEl = document.getElementById('firstQuery');
+let firstAns = document.getElementById('firstAns');
+firstEl.onclick = function(){
+firstAns.textContent = "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels."
+}
+
+
+let secondEl = document.getElementById('secondQuery');
+let secondAns = document.getElementById('secondAns');
+secondEl.onclick = function(){
+   
+    secondAns.textContent = "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!"
+}
+
+let thirdEl = document.getElementById('thirdQuery');
+let thirdAns = document.getElementById('thirdAns');
+thirdEl.onclick = function() {
+    thirdAns.textContent = " The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members."
+}
+
+
+
+
+
